629451e2519449c8621e14d626809178303daa74
kerim086 <147099168+kerim086@users.noreply.github.com>
2025-05-05 07:40:56 +0200

    amkey

---
+++ to-do-app/src/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar(){
+    return (
+      <header>
+          <nav>
+              <ul>
+                  <li><a href={"/"}>Home</a> </li>
+                  <li><a href={"/"}>React</a></li>
+                  <li><a href={"/"}>HTML</a></li>
+              </ul>
+          </nav>
+      </header>
+    );
+}
+
+export default Navbar;
