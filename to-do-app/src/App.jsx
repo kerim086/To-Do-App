57b8ea31ba50e1c4f6dc54274384b8dc7e562a66
kerim086 <147099168+kerim086@users.noreply.github.com>
2025-05-16 19:12:38 +0200

    ToDoList extension

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import ToDoList from "/pages/ToDoList.jsx"
 
 export default function App() {
   return (
     <>
+      <ToDoList/>
     </>
   )
 }
---
+++ to-do-app/src/pages/ToDoList.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+
+export default function ToDoList(){
+    const [tasks, setTasks] = useState([]);
+    const [input, setInput] = useState("");
+
+
+    return(<></>)
+}
